311a8d2b0b7a121718bc3e950b853dd0b5f1d4a7
Ervinas <ervinasma2@gmail.com>
2017-08-31 14:49:10 +0300

    Implemented FilterPanel class to Main Window class

--- LogControllerManager/src/manager/UserLogManagerMainWindow.java
+++ LogControllerManager/src/manager/UserLogManagerMainWindow.java
@@ -1,46 +1,41 @@
 package manager;
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.BoxLayout;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-
-
-import java.awt.FlowLayout;
-import javax.swing.SpringLayout;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
-import java.awt.GridBagLayout;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.awt.GridBagConstraints;
-import java.awt.Color;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
 
 public class UserLogManagerMainWindow extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
 	private JTable table_1;
+	Map<Integer, Map<String, String>> events = new HashMap<Integer, Map<String, String>>();
+	ArrayList<String> titles = new ArrayList<String>();
+	ArrayList<String> projects = new ArrayList<String>();
 
 	/**
 	 * Launch the application.
@@ -64,72 +59,41 @@ public class UserLogManagerMainWindow extends JFrame {
 	 */
 	public UserLogManagerMainWindow() throws SQLException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 756, 563);
+		//setBounds(100, 100, 756, 563);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		setSize(1200,1024);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(screenSize.width,screenSize.height);
+		setResizable(true);
 		contentPane.setLayout(null);
-		
-		Connection con = DriverManager.getConnection("jdbc:mysql://mysql:3306/logctrl?user=logctrl&password=kavospertraukele");
-		System.out.println("Connected");
-		String sql = "SELECT e.event_id, d.value, t.title, p.name\n" + 
-				"FROM event AS e\n" + 
-				"JOIN project AS p on p.project_id = e.project_id\n" +
-				"JOIN event_detail AS d on d.event_id = e.event_id\n" + 
-				"JOIN detail_titles AS t on (t.project_id = e.project_id AND t.index = d.index)" +
-				"WHERE t.project_id = 11";
-		PreparedStatement preparedStatement = con.prepareStatement(sql);
-		ResultSet rs = preparedStatement.executeQuery();
-		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(101, 41, 1000, 200);
+		scrollPane.setBounds(50, 25, screenSize.width - 100, screenSize.width/3);
 		contentPane.add(scrollPane);
 		
 		DefaultTableModel model = new DefaultTableModel(); 
 		
 		table_1 = new JTable(model);
-		scrollPane.setViewportView(table_1);
-		
-		Map<Integer, Map<String, String>> events = new HashMap<Integer, Map<String, String>>();
-		
-		while(rs.next()) {
-			int eventId = rs.getInt("event_id");
-			if (!events.containsKey(eventId)) {
-				HashMap<String, String> temp = new HashMap<String, String>();
-				temp.put("Name", rs.getString("name"));
-				temp.put("Event_id", rs.getString("event_id"));
-				events.put(eventId, temp);
-			}
-			
-			Map<String, String> event = events.get(eventId);
-			event.put(rs.getString("title"), rs.getString("value"));
-		
-		}
-		
-		String getTitles = "SELECT DISTINCT(title) FROM detail_titles";
-		PreparedStatement preparedStatement1 = con.prepareStatement(getTitles);
-		ResultSet rs2 = preparedStatement1.executeQuery();
-
-		ArrayList<String> titles = new ArrayList<String>();
-		
-		titles.add("Name");
-		titles.add("Event_id");
-		
-		model.addColumn("Name");
-		model.addColumn("Event_id");
-		
-		while(rs2.next()) {
-			String title = rs2.getString("title");
-			titles.add(title);
-			model.addColumn(title);
-		}
-		
+		scrollPane.setViewportView(table_1);		
 		
 
+		//------------------------------------------------
+		getColumnNamesToPanel(model);
+		this.projects = AddLogsToArrayReturnProjectNames();
+		fillDataToPanel(model);
+		//------------------------------------------------	
+		
+		FilterPanel filterPanel = new FilterPanel(this.titles, this.projects);
+		filterPanel.setSize(263, 275);
+		filterPanel.setLocation(50, 517);
+		contentPane.add(filterPanel);		
+		
+		
+		
+		
+	}
 	
-	
-		System.out.println(titles.size());
+	private void fillDataToPanel(DefaultTableModel model) {
 		
 		Set<Integer> keys = events.keySet();
 		Iterator<Integer> iter = keys.iterator();
@@ -137,9 +101,9 @@ public class UserLogManagerMainWindow extends JFrame {
 		while(iter.hasNext()) {
 			int eventID = iter.next();
 			Map<String, String> event = events.get(eventID);
-			Object[] row = new Object[titles.size()];
-			for (int x = 0; x < titles.size(); x++) {
-				row[x] = event.get(titles.get(x));
+			Object[] row = new Object[this.titles.size()];
+			for (int x = 0; x < this.titles.size(); x++) {
+				row[x] = event.get(this.titles.get(x));
 			}
 //			row[0] = "foo";
 //			model.addRow(new Object[] {
@@ -155,6 +119,73 @@ public class UserLogManagerMainWindow extends JFrame {
 //					event.get("user-agent")
 //				});
 			model.addRow(row);
-		}	
+		}
+		
+	}
+	
+	private void getColumnNamesToPanel(DefaultTableModel model) throws SQLException {
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://mysql:3306/logctrl?user=logctrl&password=kavospertraukele");
+		String getTitles = "SELECT DISTINCT(title) FROM detail_titles";
+		PreparedStatement preparedStatement1 = con.prepareStatement(getTitles);
+		ResultSet rs2 = preparedStatement1.executeQuery();
+
+		//ArrayList<String> titles = new ArrayList<String>();
+		
+		this.titles.add("Name");
+		this.titles.add("Event_id");
+		
+		model.addColumn("Name");
+		model.addColumn("Event_id");
+		
+		while(rs2.next()) {
+			String title = rs2.getString("title");
+			this.titles.add(title);
+			model.addColumn(title);
+		}
+		
+		rs2.close();
+		con.close();
+		
+	}
+	
+	private ArrayList AddLogsToArrayReturnProjectNames() throws SQLException {
+		
+		ArrayList<String> clientProjects = new ArrayList<String>();
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://mysql:3306/logctrl?user=logctrl&password=kavospertraukele");
+		System.out.println("Connected");
+		String sql = "SELECT e.event_id, d.value, t.title, p.name\n" + 
+				"FROM event AS e\n" + 
+				"JOIN project AS p on p.project_id = e.project_id\n" +
+				"JOIN event_detail AS d on d.event_id = e.event_id\n" + 
+				"JOIN detail_titles AS t on (t.project_id = e.project_id AND t.index = d.index)";
+				//"WHERE t.project_id = 11";
+		PreparedStatement preparedStatement = con.prepareStatement(sql);
+		ResultSet rs = preparedStatement.executeQuery();
+		
+		while(rs.next()) {
+			int eventId = rs.getInt("event_id");
+			if (!this.events.containsKey(eventId)) {
+				HashMap<String, String> temp = new HashMap<String, String>();
+				
+				temp.put("Name", rs.getString("name"));
+				temp.put("Event_id", rs.getString("event_id"));
+				this.events.put(eventId, temp);
+			}
+			if( !clientProjects.contains(rs.getString("Name"))) {
+				clientProjects.add(rs.getString("Name"));
+			}
+			
+			Map<String, String> event = this.events.get(eventId);
+			event.put(rs.getString("title"), rs.getString("value"));
+		
+		}
+		
+		rs.close();
+		con.close();
+		
+		return clientProjects;
+	
 	}
 }
